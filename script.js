386b2c7267480b522fc816da18da2364380430bd
rektbyminercz <minerhraje@email.cz>
2025-02-13 19:48:49 +0100

    commitloginu

---
+++ script.js
@@ -0,0 +1,13 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const passwordIcon = document.querySelector('.show-password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        passwordIcon.classList.remove('fa-eye');
+        passwordIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        passwordIcon.classList.remove('fa-eye-slash');
+        passwordIcon.classList.add('fa-eye');
+    }
+}
